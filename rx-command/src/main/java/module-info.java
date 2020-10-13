61e87fb993ef6e07431af76d8a19d582a1b5b594
Knut-Helge Vik <knuthelge.vik@gmail.com>
2020-10-13 08:51:45 +0200

    working pom files

--- rx-command/src/main/java/module-info.java
+++ rx-command/src/main/java/module-info.java
@@ -11,38 +11,57 @@ module rx.command.main {
     exports com.intact.rx.api.rxrepo;
     exports com.intact.rx.api.subject;
 
-    exports com.intact.rx.core.cache.data.id;
     exports com.intact.rx.core.cache;
-    exports com.intact.rx.core.cache.data.context;
-    exports com.intact.rx.core.cache.factory;
     exports com.intact.rx.core.cache.data;
+    exports com.intact.rx.core.cache.data.id;
+    exports com.intact.rx.core.cache.data.context;
+    exports com.intact.rx.core.cache.data.api;
+    exports com.intact.rx.core.cache.factory;
+    exports com.intact.rx.core.cache.nullobjects;
+    exports com.intact.rx.core.cache.status;
+    exports com.intact.rx.core.cache.strategy;
+    exports com.intact.rx.core.cache.subject;
+
+    exports com.intact.rx.core.command;
+    exports com.intact.rx.core.command.action;
+    exports com.intact.rx.core.command.api;
+    exports com.intact.rx.core.command.factory;
+    exports com.intact.rx.core.command.nullobjects;
+    exports com.intact.rx.core.command.observer;
+    exports com.intact.rx.core.command.result;
+    exports com.intact.rx.core.command.status;
+    exports com.intact.rx.core.command.strategy;
+
+    exports com.intact.rx.core.machine;
+    exports com.intact.rx.core.machine.api;
+    exports com.intact.rx.core.machine.context;
+    exports com.intact.rx.core.machine.factory;
+
+    exports com.intact.rx.core.rxcache;
+    exports com.intact.rx.core.rxcache.act;
+    exports com.intact.rx.core.rxcache.acts;
+    exports com.intact.rx.core.rxcache.api;
+    exports com.intact.rx.core.rxcache.controller;
+    exports com.intact.rx.core.rxcache.factory;
+    exports com.intact.rx.core.rxcache.noop;
+
+    exports com.intact.rx.core.rxcircuit.breaker;
+    exports com.intact.rx.core.rxcircuit.rate;
+
+    exports com.intact.rx.core.rxrepo;
+    exports com.intact.rx.core.rxrepo.factory;
+    exports com.intact.rx.core.rxrepo.noop;
+    exports com.intact.rx.core.rxrepo.reader;
+    exports com.intact.rx.core.rxrepo.writer;
 
     exports com.intact.rx.exception;
+
+    exports com.intact.rx.monitor;
+
     exports com.intact.rx.policy;
+
     exports com.intact.rx.templates;
     exports com.intact.rx.templates.annotations;
     exports com.intact.rx.templates.api;
     exports com.intact.rx.templates.key;
-
-    exports com.intact.rx.core.rxcircuit.breaker;
-    exports com.intact.rx.core.rxcircuit.rate;
-    //provides RxRepository with CachedRepository;
-
-    exports com.intact.rx.core.rxcache.acts;
-    exports com.intact.rx.core.rxcache.act;
-    exports com.intact.rx.core.command;
-    exports com.intact.rx.core.machine.context;
-    exports com.intact.rx.core.rxcache.controller;
-
-    exports com.intact.rx.core.command.api;
-    exports com.intact.rx.core.rxrepo;
-    exports com.intact.rx.core.command.status;
-    exports com.intact.rx.core.cache.status;
-    exports com.intact.rx.core.cache.subject;
-    exports com.intact.rx.core.command.observer;
-    exports com.intact.rx.core.machine;
-    exports com.intact.rx.core.rxcache.api;
-    exports com.intact.rx.core.rxcache.factory;
-    exports com.intact.rx.core.rxcache;
-    exports com.intact.rx.core.machine.api;
 }
